294beb15854624e9a703c48ee6d71540782af565
SUYASH2004 <ranesuyash2004@gmail.com>
2025-08-05 20:20:15 +0530

    Removed xyz.py

--- xyz.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_KEY = 'AIzaSyB9L63ksh-IcbZl7su-W43VyZ0O4_8dbd4'
-
-url = f'https://generativelanguage.googleapis.com/v1/models?key={API_KEY}'
-
-response = requests.get(url)
-print(response.json())
